35a46a5b03cee786ae57f9e12e05d0a0981bc303
mDeanHaj <mhajjar@csumb.edu>
2023-04-08 21:01:29 -0700

    update
    finished registration page

--- app/src/main/java/com/example/moviemart/LoginActivity.java
+++ app/src/main/java/com/example/moviemart/LoginActivity.java
@@ -3,12 +3,62 @@ package com.example.moviemart;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import com.example.moviemart.db.UserDao;
+import com.example.moviemart.db.UserDatabase;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText userId, password;
+    Button register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        userId = findViewById(R.id.userId);
+        password = findViewById(R.id.password);
+        register = findViewById(R.id.register);
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                User user = new User();
+                user.setUserId(userId.getText().toString());
+                user.setPassword(password.getText().toString());
+
+                if(validateInput(user)){
+                    UserDatabase userDatabase = UserDatabase.getUserDatabase(getApplicationContext());
+                    UserDao userDao = userDatabase.mUserDao();
+                    new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            userDao.registerUser(user);
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    Toast.makeText(getApplicationContext(), "Username Registered!", Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                        }
+                    }).start();
+                }else{
+                    Toast.makeText(getApplicationContext(), "Fill Username and Password!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private Boolean validateInput(User user){
+        if(user.getUserId().isEmpty() || user.getPassword().isEmpty()){
+            return false;
+        }
+        return true;
     }
 }
