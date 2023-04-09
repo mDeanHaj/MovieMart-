166a8446c0b62b1fcaaeea5a7879916eefedb910
mDeanHaj <mhajjar@csumb.edu>
2023-04-09 11:37:44 -0700

    Revert "Revert "update""
    This reverts commit 1d8bff29db40f8422a934083ab57c98b83578cfc.

--- app/src/main/java/com/example/moviemart/LoginActivity.java
+++ app/src/main/java/com/example/moviemart/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.moviemart;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -14,7 +15,8 @@ import com.example.moviemart.db.UserDatabase;
 public class LoginActivity extends AppCompatActivity {
 
     EditText userId, password;
-    Button login;
+    Button regUser, login;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +25,10 @@ public class LoginActivity extends AppCompatActivity {
 
         userId = findViewById(R.id.userId);
         password = findViewById(R.id.password);
+        regUser = findViewById(R.id.regUser);
         login = findViewById(R.id.login);
+
+
 
         login.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -47,11 +52,19 @@ public class LoginActivity extends AppCompatActivity {
                                     }
                                 });
                             }else{
-                                //here
+                                String name = user.userId;
+                                startActivity(new Intent(LoginActivity.this, Dashboard.class)
+                                        .putExtra("name", name));
                             }
                         }
                     }).start();
                 }
+            }
+        });
+        regUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
             }
         });
     }
