de2c152b71eecb535a61b77ea7c850a119c92452
mDeanHaj <mhajjar@csumb.edu>
2023-04-11 00:12:56 -0700

    WORKING
    fixed bugs

--- app/src/main/java/com/example/moviemart/AdminActivity.java
+++ app/src/main/java/com/example/moviemart/AdminActivity.java
@@ -2,13 +2,27 @@ package com.example.moviemart;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AdminActivity extends AppCompatActivity {
+
+    Button mReturnButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
+
+        mReturnButton = findViewById(R.id.returnButton);
+
+        mReturnButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(AdminActivity.this, Dashboard.class));
+            }
+        });
     }
 }
