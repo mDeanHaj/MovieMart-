bc8d496effb0d573a182be79e60a496947673cf4
mDeanHaj <mhajjar@csumb.edu>
2023-04-13 00:55:54 -0700

    update
    - make each movie purchase-able

--- app/src/main/java/com/example/moviemart/SearchMovie.java
+++ app/src/main/java/com/example/moviemart/SearchMovie.java
@@ -1,20 +1,23 @@
 package com.example.moviemart;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.example.moviemart.db.MovieDatabase;
 
 import java.util.List;
 
-public class SearchMovie extends AppCompatActivity {
+public class SearchMovie extends AppCompatActivity implements MovieAdapter.OnItemClickListener {
 
     private RecyclerView recyclerView;
     private MovieAdapter adapter;
     private MovieDatabase movieDatabase;
+    private List<Movie> movies;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,15 +28,34 @@ public class SearchMovie extends AppCompatActivity {
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setHasFixedSize(true);
 
-        adapter = new MovieAdapter();
+        adapter = new MovieAdapter(this, this);
         recyclerView.setAdapter(adapter);
 
         movieDatabase = MovieDatabase.getInstance(this);
         MovieDatabaseInitializer.populateAsync(movieDatabase);
 
         new Thread(() -> {
-            List<Movie> movies = movieDatabase.movieDao().getAllMovies();
+            movies = movieDatabase.movieDao().getAllMovies();
             runOnUiThread(() -> adapter.setMovies(movies));
         }).start();
+    }
+
+    @Override
+    public void onItemClick(int position) {
+        Movie selectedMovie = movies.get(position);
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Order Movie");
+        builder.setMessage("Would you like to order " + selectedMovie.getTitle() + "?");
+        builder.setPositiveButton("Yes", (dialog, which) -> {
+            // Perform action when the user clicks "Yes"
+            Toast.makeText(SearchMovie.this, "You have ordered " + selectedMovie.getTitle(), Toast.LENGTH_SHORT).show();
+        });
+        builder.setNegativeButton("No", (dialog, which) -> {
+            // Perform action when the user clicks "No"
+            Toast.makeText(SearchMovie.this, "You have not ordered " + selectedMovie.getTitle(), Toast.LENGTH_SHORT).show();
+        });
+
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
     }
 }
