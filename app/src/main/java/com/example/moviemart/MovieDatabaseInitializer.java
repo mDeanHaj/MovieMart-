1e5fcaa3e608fb848c59a780c75959460b938c6b
mDeanHaj <mhajjar@csumb.edu>
2023-04-14 17:05:48 -0700

    Update
    - added movies to list

--- app/src/main/java/com/example/moviemart/MovieDatabaseInitializer.java
+++ app/src/main/java/com/example/moviemart/MovieDatabaseInitializer.java
@@ -20,11 +20,12 @@ public class MovieDatabaseInitializer {
     public static void populateSync(MovieDatabase movieDatabase) {
         MovieDao dao = movieDatabase.movieDao();
 
-        insertIfNotExists(dao, new Movie("The Shawshank Redemption", 1994, "Drama", 142));
+        insertIfNotExists(dao, new Movie("Spider-Man", 2002, "Action, Science Fiction", 121));
         insertIfNotExists(dao, new Movie("The Godfather", 1972, "Crime, Drama", 175));
-        insertIfNotExists(dao, new Movie("The Dark Knight", 2008, "Action, Crime, Drama", 152));
+        insertIfNotExists(dao, new Movie("The Dark Knight", 2008, "Action, Crime", 152));
         insertIfNotExists(dao, new Movie("Pulp Fiction", 1994, "Crime, Drama", 154));
         insertIfNotExists(dao, new Movie("Fight Club", 1999, "Drama", 139));
+        insertIfNotExists(dao, new Movie("Harry Potter and the Sorcerer's Stone", 2001, "Fantasy, Adventure", 152));
     }
 
     private static void insertIfNotExists(MovieDao dao, Movie movie) {
